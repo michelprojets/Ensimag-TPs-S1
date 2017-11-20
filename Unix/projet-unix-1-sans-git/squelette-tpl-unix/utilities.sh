140706bce3ab05464072079a189e65525d38f768
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2017-11-20 12:51:56 +0100

    ajout projet unix 1 sans git

---
+++ Unix/projet-unix-1-sans-git/squelette-tpl-unix/utilities.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+# genere l'entete de page HTML en sortie standard
+html_head () {
+    echo "<!DOCTYPE html>"
+    echo "<html>"
+    echo "<head>"
+    echo "<meta charset=\"UTF-8\">"
+    echo "<title>"$1"</title>"
+    echo "</head>"
+    echo "<body>"
+}
+
+# genere le titre de page HTML en sortie standard
+html_title () {
+    echo "<h1>"$1"</h1>"
+}
+
+# genere le pied de page HTML en sortie standard
+html_tail () {
+    echo "</body>"
+    echo "</html>"
+}
+
+# genere un bloc image de la page HTML en sortie standard
+generate_img_fragment () {
+    case "$1" in
+        ?*.jpg)
+            echo "<div style=\"float:left\">"
+            echo "<img src=\"$1\" alt=\"$1\"><br>"
+            echo "</div>"
+            ;;
+        *)
+            echo "Vous devez mettre un fichier .jpg"
+            ;;
+    esac
+}
+
+# genere un fichier index.html dans le repertoire cible
+galerie_main () {
+    echo "appel de la fonction galerie_main avec les parametres $1 et $2"
+}
