1a417a7b6b2833ed6ceaa5ff47325e207c608297
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2018-02-09 15:13:09 +0100

    modification projet c exo morse

--- Projet_C/stage_C/exos-avance/morse/test_morse.c
+++ Projet_C/stage_C/exos-avance/morse/test_morse.c
@@ -7,8 +7,13 @@ int main()
 {
     help(); // Affiche la correspondance des caracteres pris en charge et des codes Morse
     printf("Affichage du code Morse de la phrase : \"RMS Titanic SOS\"\n");
-    affiche_morse("RMS Titanic SOS5");
+    affiche_morse("RMS Titanic SOS");
+    // char texte[] = "RMS Titanic SOS5";
+    // affiche_morse(texte);
     printf("Affichage de la traduction du code Morse : \".-. -- ... // - .. - .- -. .. -.-. // ... --- ... // .---- ..... // .- ...- .-. .. .-.. // .---- ---. .---- ..---\"\n");
     affiche_texte(".-. -- ... //  - .. - .- -. .. -.-. // ... --- ... // .---- ..... // .- ...- .-. .. .-.. // .---- ---. .---- ..---");
+    // mieux et en plus, POUR EVITER DE FAIRE UNE COPIE DE LA CHAINE DANS affiche_texte()
+    // char morse[] = ".-. -- ... //  - .. - .- -. .. -.-. // ... --- ... // .---- ..... // .- ...- .-. .. .-.. // .---- ---. .---- ..---";
+    // affiche_texte(morse);
     return EXIT_SUCCESS;
 }
