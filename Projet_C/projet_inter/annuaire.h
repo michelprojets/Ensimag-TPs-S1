27da7eddf932a89606fa9a8e8788c9001687ae27
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2018-03-05 14:53:06 +0100

    commencement projet inter C annuaire

---
+++ Projet_C/projet_inter/annuaire.h
@@ -0,0 +1,45 @@
+#ifndef _ANNUAIRE_H_
+#define _ANNUAIRE_H_
+
+/**
+ *Creation d'un annuaire initialement vide
+ */
+extern struct annuaire * creer();
+
+/**
+ *Insere une nouvelle entree nom/numero dans l'annuaire
+ *@param[in-out] an : l'annuaire
+ *@param[in] nom : le nom de la personne
+ *@param[in] numero : le numero de telephone
+ *@return NULL si l'annuaire ne contenait pas le nom, l'ancien numero correspondant au nom sinon
+ *@pre an != NULL && nom != "" && numero != ""
+ *@post la chaine retournee doit etre liberee en memoire
+ */
+extern char * inserer(struct annuaire * an, const char * nom, const char * numero);
+
+/**
+ *Recherche dans l'annuaire le numero de telephone associe au nom de la personne
+ *@param[in-out] an : l'annuaire
+ *@param[in] nom : le nom de la personne
+ *@return une copie du numero de telephone associe au nom de la personne
+ *@pre an != NULL && nom != ""
+ */
+extern char * rechercher_numero(struct annuaire * an, const char * nom);
+
+/**
+ *Supprime l'entree nom/numero associee au nom, si le nom n'existe pas, ne fait rien
+ *@param[in-out] an : l'annuaire
+ *@param[in] nom : le nom de la personne
+ *@pre an != NULL && nom != ""
+ */
+extern void supprimer(struct annuaire * an, const char * nom);
+
+/**
+ *Supprime/libere toutes les entrees de l'annuaire et l'annuaire lui-meme
+ *@param[in-out] an : l'annuaire
+ *@pre an != NULL
+ */
+extern void liberer(struct annuaire * an);
+
+
+#endif  /* _ANNUAIRE_H_ */
