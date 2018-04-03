8a157e0d1c632463a92e842be122d45f674835cd
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2018-04-04 00:35:48 +0200

    projet stats

--- Statistiques/projet/premiere_strategie.R
+++ Statistiques/projet/premiere_strategie.R
@@ -9,10 +9,20 @@ moy_emp<-sum(simu_binom)/n
 var_emp<-sum((simu_binom-moy_emp)^2)/n
 moy_theo<-p
 var_theo<-(1-p)*p
+print("moyenne empirique :")
 print(moy_emp)
+print("moyenne théorique :")
 print(moy_theo)
+print("variance empirique :")
 print(var_emp)
+print("variance empirique :")
 print(var_theo)
+print("nombre de poissons bagués parmi les n poissons pêchés :")
 t=sum(simu_binom)
 print(t)
-
+estim_mom<-sum(simu_binom)/n
+estim_vrai<-sum(simu_binom)/n
+print("estimateur des moments :")
+print(estim_mom)
+print("estimateur de maximum de vraisemblance :")
+print(estim_vrai)
