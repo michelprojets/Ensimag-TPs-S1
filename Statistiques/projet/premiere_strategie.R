10d98f63a9738ecc049bd1f9f797e3240b9ed6c4
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2018-04-04 20:56:21 +0200

    projet stats

--- Statistiques/projet/premiere_strategie.R
+++ Statistiques/projet/premiere_strategie.R
@@ -4,7 +4,10 @@ teta<-1000
 n0<-50
 n<-60 # valeur arbitraire
 p<-n0/teta
-simu_binom<-rbinom(n, 1, p)
+# simu_binom<-rbinom(n, 1, p) # pour récuperer un échantillon
+simu_binom<-c(0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+print("échantillon :")
+print(simu_binom)
 moy_emp<-sum(simu_binom)/n
 var_emp<-sum((simu_binom-moy_emp)^2)/n
 moy_theo<-p
@@ -20,8 +23,8 @@ print(var_theo)
 print("nombre de poissons bagués parmi les n poissons pêchés :")
 t=sum(simu_binom)
 print(t)
-estim_mom<-sum(simu_binom)/n
-estim_vrai<-sum(simu_binom)/n
+estim_mom<-(n0*n)/sum(simu_binom)
+estim_vrai<-(n0*n)/sum(simu_binom)
 print("estimateur des moments :")
 print(estim_mom)
 print("estimateur de maximum de vraisemblance :")
