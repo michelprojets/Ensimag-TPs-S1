01fd5147aa686656c98660cd97a3dee90e7df843
Michel Yoeung <Michel.Yoeung@grenoble-inp.orgs>
2018-04-13 11:02:16 +0200

    projet stats

---
+++ Statistiques/projet/scripts/exercice_3.R
@@ -0,0 +1,91 @@
+
+#donnees = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
+
+n0 = 100
+
+Nombre_poissons_bagués = sum(donnees)
+
+print(Nombre_poissons_bagués)
+n = length(donnees)
+ratio = Nombre_poissons_bagués / n
+
+Théta_estimé = 100 / ratio
+
+print(ratio)
+print(Théta_estimé)
+
+
+# Calculons les intevalles de confiance exact puis asymptotiques :
+
+alpha = 0.05 # On définit alpha à 5%
+
+Ice_inf = 1 / (1 + (n - Nombre_poissons_bagués +1)/ Nombre_poissons_bagués * qf(1-alpha / 2, 2 * (n - Nombre_poissons_bagués +1), 2 * Nombre_poissons_bagués))
+
+Ice_sup = 1 / (1 + (n - Nombre_poissons_bagués)/ (Nombre_poissons_bagués + 1) * qf(1-alpha / 2, 2 * (n - Nombre_poissons_bagués), 2 * (Nombre_poissons_bagués + 1)))
+
+u_alpha = qnorm (1 - alpha)
+
+racine = sqrt(ratio * (1 - ratio) / n)
+
+Ica_inf = ratio - u_alpha * racine
+
+Ica_sup = ratio + u_alpha * racine
+  
+Ice = c(Ice_inf, Ice_sup)
+
+Ica = c(Ica_inf, Ica_sup)
+
+print(Ice)
+print(Ica)
+
+
+# Question 2
+
+
+vecteur_y <- function(donnees, Nombre_poissons_bagués) {
+  
+  Y <- rep(0, Nombre_poissons_bagués)
+  nombre = 1
+  indice = 1
+  for (bague in donnees) {
+    if (bague) {
+      Y[indice] = nombre
+      nombre = 1
+      indice = indice + 1
+    }
+    else {
+      nombre = nombre + 1
+    }
+  }
+  return(Y)
+}
+
+Y = vecteur_y(donnees, Nombre_poissons_bagués)
+
+print(Y)
+
+moyenne = sum(Y) / length(Y)
+
+# Le paramètre appelé "lambda" de la loi géométrique est l'inverse de l'espérence. 
+# La moyenne que l'on vient de calculer correspond à l'espérence de notre tirage.
+
+lambda = 1 / moyenne
+
+print(lambda)
+
+# Calculons l'intervallle de confiance asymptotique de seuil 5%
+# alpha et u_alpha ont déjà été définis à 0.05 plus haut.
+
+# Le calcul de lambda_inf donne un résultat négatif, donc on rendra plutôt 0 :
+lambda_inf = max(lambda - u_alpha * sqrt(lambda * (1 - lambda) / length(Y)), 0)
+
+lambda_sup = lambda + u_alpha * sqrt(lambda * (1 - lambda) / length(Y))
+
+Intervalle = c (lambda_inf, lambda_sup)
+
+print(Intervalle)
+
+
+# QUESTION 3 :
+
+hist(Y, main = "Temps d'attente avant de pêcher le premier poisson bagué", xlab = "Y", ylab = "Occurences", nclass = 5)
