184aaf0181051f40002f4e51c389fdc263606af8
michel <michelyoeung@hotmail.fr>
2018-04-18 21:54:00 +0200

    projet algo

--- Algorithmique/projet/fichiers_fournis/geo/graph.py
+++ Algorithmique/projet/fichiers_fournis/geo/graph.py
@@ -6,6 +6,7 @@ from geo.quadrant import Quadrant
 from geo.union import UnionFind
 from geo.segment import Segment
 from geo.hash import ordered_segments
+from geo.tycat import tycat # pour le deboggage
 
 class Graph:
     """
@@ -43,7 +44,41 @@ class Graph:
         if hash_points is true then use hashed segments iterator
         else use quadratic segments iterator.
         """
-        pass
+        points = list(self.vertices.keys())
+        related_components = UnionFind()
+
+        def linked(vertice1, vertice2):
+            """
+            return true if both vertices are linked in the graph, else false
+            """
+            for segment1, segment2 in zip(self.vertices[vertice1], self.vertices[vertice2]):
+                if (segment1.endpoint_not(vertice1) == vertice2 or
+                        segment2.endpoint_not(vertice2) == vertice1):
+                    return True
+            return False
+
+        # création de la structure modélisant les composantes connexes du graphe
+        for i in range(len(points)):
+            related_components.add(points[i])
+            for j in range(i):
+                if linked(points[i], points[j]):
+                    related_components.union(points[i], points[j])
+                    break
+
+        if related_components.size == 1:
+            return
+        for segment in ordered_segments(hash_points, points):
+            # si les deux points appatiennent à deux composantes différentes
+            if (related_components.find(segment.endpoints[0]) !=
+                    related_components.find(segment.endpoints[1])):
+                # ajout du segment aux arêtes du graphe
+                self.vertices[segment.endpoints[0]].append(segment)
+                self.vertices[segment.endpoints[1]].append(segment)
+                # fusion des deux composantes connexes
+                related_components.union(segment.endpoints[0], segment.endpoints[1])
+            if related_components.size == 1:
+                return
+
 
     def even_degrees(self, hash_points):
         """
@@ -51,7 +86,23 @@ class Graph:
         if hash_points is true then use hashed segments iterator
         else use quadratic segments iterator.
         """
-        pass
+        points = list(self.vertices.keys())
+        unevens = 0
+        for vertice in points:
+            if len(self.vertices[vertice])%2 != 0:
+                unevens += 1
+        if unevens == 0:
+            return
+        for segment in ordered_segments(hash_points, points):
+            if (len(self.vertices[segment.endpoints[0]])%2 != 0 and
+                    len(self.vertices[segment.endpoints[0]])%2 != 0):
+                # ajout du segment aux arêtes du graphe
+                self.vertices[segment.endpoints[0]].append(segment)
+                self.vertices[segment.endpoints[1]].append(segment)
+                unevens -= 2
+                if unevens == 0:
+                    return
+
 
     def eulerian_cycle(self):
         """
--- Algorithmique/projet/fichiers_fournis/geo/hash.py
+++ Algorithmique/projet/fichiers_fournis/geo/hash.py
@@ -1,7 +1,7 @@
 """
 hashtable structure
 """
-from geo.point import Point
+from itertools import combinations
 from geo.segment import Segment
 
 class Hash:
@@ -9,66 +9,86 @@ class Hash:
     hash class
     """
 
-def insert_into_table(tables, index, hash_key, point):
-    """
-    sub-function for hasher
-    """
-    if hash_key in tables[index].keys():
-        tables[index].add(point)
-    else:
-        tables[index].update({hash_key : list(point)})
+    def __init__(self, points, precision):
+        """
+        constructor
+        """
+        self.tables = list(dict() for _ in range(4))
+        self.hasher(points, precision)
 
-def hasher(points, precision):
-    # on pouvait hasher un par un pour ne pas avoir à faire les 4 à chaque fois même dans le cas ou ce n'est pas nécessaire
-    """
-    hasher function
-    """
-    tables = list(dict() for _ in range(4))
-    for point in points:
-        hash_keys = list((point.coordinates[0]//precision,
+    def insert_into_table(self, index, hash_key, point):
+        """
+        sub-function for hasher
+        """
+        if hash_key not in self.tables[index].keys():
+            self.tables[index][hash_key] = []
+        self.tables[index][hash_key].append(point)
+
+    def hasher(self, points, precision):
+        # on pouvait hasher un par un pour ne pas avoir à faire les 4 à chaque
+        # fois même dans le cas ou ce n'est pas nécessaire
+        """
+        hasher function
+        """
+        for point in points:
+            # les 4 fonctions de hashage directement appliquées
+            hash_keys = [(point.coordinates[0]//precision,
                           point.coordinates[1]//precision),
                          ((point.coordinates[0]+(precision/2))//precision,
                           point.coordinates[1]//precision),
                          (point.coordinates[0]//precision,
                           (point.coordinates[1]+(precision/2))//precision),
                          ((point.coordinates[0]+(precision/2))//precision,
-                          (point.coordinates[1]+(precision/2))//precision))
-        for index in range(len(tables)):
-            insert_into_table(tables, index, hash_keys[index], point)
-    return tables
+                          (point.coordinates[1]+(precision/2))//precision)]
+            for index in range(len(self.tables)):
+                self.insert_into_table(index, hash_keys[index], point)
 
-def has_collision(table):
-    """
-    return true if hash table given has collision, else false
-    """
-    for points in table.values():
-        if len(points) > 1:
-            return True
-    return False
+    def has_collision(self, index):
+        """
+        return true if hash table corresponding to the index given has collision, else false
+        """
+        for points in self.tables[index].values():
+            if len(points) > 1:
+                return True
+        return False
 
-def ordered_segments(points):
+def ordered_segments(hash_points, points):
     """
     iterator for ordered segments
     """
-    precision = 0.5
-    tables_groups = list(hasher(points, precision))
-    while True: # pour simuler un do while
-        collision = False
-        for index in range(len(tables_groups[0])):
-            if has_collision(tables_groups[-1][index]):
-                collision = True
+    # avec hashage
+    if hash_points:
+        precision = 2.0 # valeur arbitraire
+        tables_groups = [Hash(points, precision)]
+        while True: # pour simuler un do while
+            collision = False
+            for index in range(len(tables_groups[0].tables)):
+                if tables_groups[-1].has_collision(index):
+                    collision = True
+                    break
+            if collision:
+                precision = precision/2
+                # on pouvait faire en sorte de ne hasher que les tables
+                # qui possèdent une collision et pas les 4 toujours
+                tables_groups.append(Hash(points, precision))
+            else:
                 break
-        if collision:
-            precision = precision/2
-            # on pouvait faire en sorte de ne hasher que les tables qui possèdent une collision et pas les 4 toujours
-            tables_groups.append(hasher(points, precision))
-        else:
-            break
-    for tables in reversed(tables_groups):
-        for table in tables:
-            for key in table.keys():
-                for points in table[key]:
-                    if len(points) > 1:
-                        for i in range(len(points)):
-                            for j in range(i+1, len(points)):
-                                yield Segment([points[i], points[j]])
+        for hash_tables in reversed(tables_groups):
+            for hash_table in hash_tables.tables:
+                for hash_points in hash_table.values():
+                    if len(hash_points) > 1:
+                        # toutes les combinaisons de points
+                        for point1, point2 in combinations(hash_points, 2):
+                            yield Segment([point1, point2])
+    # sans hachage
+    else:
+        for point1, point2 in combinations(points, 2):
+            yield Segment([point1, point2])
+
+        # # coût asymptôtique trop élevé
+        # segments = list()
+        # for point1, point2 in combinations(points, 2):
+        #     segments.append(Segment([point1, point2]))
+        # segments = sorted(segments, key=segments[0].length())
+        # for segment in segments:
+        #     yield segment
